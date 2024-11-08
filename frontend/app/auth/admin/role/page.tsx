9e0d9b68a3cb8b0499f09a707d84ddfee9663760
JimmyQYHuang <jimmyqyhuang@ami.com>
2024-11-08 19:12:45 +0800

    backup

--- frontend/app/auth/admin/role/page.tsx
+++ frontend/app/auth/admin/role/page.tsx
@@ -138,7 +138,7 @@ export default function Page() {
                     onClick={() => handleSort(column as keyof AccountData)}
                     className={cn("h-10 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]")}
                   >
-                    {column}
+                    {column} {sortColumn === column ? (sortDirection === "asc" ? "▲" : "▼") : ""}
                   </th>
                 ))}
               </tr>
